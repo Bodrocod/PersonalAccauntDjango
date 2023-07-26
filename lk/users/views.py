65076a7c873019936192f936579a0a71f9ba915d
Maks Bodrov <bodrocod@gmail.com>
2023-07-26 22:47:54 +0300

    Установлен Djangorestframework
    и произведены первоначальные настройки

--- lk/users/views.py
+++ lk/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import CustomUserSerializer
+from .models import CustomUser
 
 # Create your views here.
+
+
+class CustomUsrViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [permissions.IsAdminUser]
